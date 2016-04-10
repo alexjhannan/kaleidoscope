73ed9ea92bd86a2e3f1ae88caaa546df4f2a0c68
alexjhannan <alexjhannan@gmail.com>
2016-04-10 12:31:41 -0400

    Removed starter components

--- client/components/starter.jsx
+++
@@ -1,8 +0,0 @@
-// starter component - temporary, used to get structure up and running
-import React from 'react';
-
-export default Starter = () => (
-	<div>
-		<h3>Starter Component</h3>
-	</div>
-);
--- client/layouts/starterLayout.jsx
+++
@@ -1,8 +0,0 @@
-// starterLayout - layout to get the structure going
-import React from 'react';
-
-export default StarterLayout = ({starter}) => (
-	<div>
-		{starter}
-	</div>
-)
