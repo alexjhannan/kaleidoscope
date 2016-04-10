511de107286c17a6a29b35d5d5ef9e5247139f66
alexjhannan <alexjhannan@gmail.com>
2016-04-10 15:53:12 -0400

    Reskinned navbar for Kaleidoscope

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 export default Navbar = () =>(
-  <nav className="light-blue lighten-1" role="navigation">
-    <div className="nav-wrapper container"><a id="logo-container" href="#" className="brand-logo">Logo</a>
+  <nav className="purple darken-4 small" role="navigation">
+    <div className="nav-wrapper container">
+      <a id="logo-container" href="#" className="brand-logo">Kaleidoscope</a>
+      
       <ul className="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="#">Link</a></li>
       </ul>
 
       <ul id="nav-mobile" className="side-nav">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="#">Link</a></li>
       </ul>
+
       <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
+    
     </div>
   </nav>
 );
