2a1b9bebf37bb7a3b57d61c9025449090d2d5bdc
alexjhannan <alexjhannan@gmail.com>
2016-04-16 11:37:14 -0400

    Wiseman starter component finished

--- client/components/clock.jsx
+++ client/components/clock.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default Clock = ({time, msg}) => {
-	return (<div><div>{time}</div> <div>{msg}</div></div>)
+export default Clock = ({time, msg, count}) => {
+	let message
+
+	msg ? message = <div className="center">{msg}</div> : '';
+
+	return (<div><div className="center">{time}</div> {message} <div className="center">{count}</div></div>)
 }
--- client/containers/clock.jsx
+++ client/containers/clock.jsx
@@ -1,28 +1,53 @@
 import {compose} from 'react-komposer';
+import moment from 'moment';
 import ClockComp from '/client/components/clock.jsx';
 
+let possibleMessages = [
+	'You can do it!',
+	'The stars are only so far away.',
+	'Love thyself.',
+	'Burn brilliantly.',
+	'Absorb and reflect.',
+	'Build yourself brick by brick.',
+	'Enjoy the little things.',
+	'Embrace failure.',
+];
+
 const composerFxn = (props, onData) => {
-	let counter = 0;
-	let msg = 'hi';
+	let counter = 0, randomIndex, msg, count;
 
-	const handler = setInterval(() => {
-		console.log(counter);
+	let setMessage = () => {
+		randomIndex = Math.floor(Math.random()*possibleMessages.length);
+		msg = possibleMessages[randomIndex];
+	}
+
+	const messageSetter = setInterval(() => {
+			setMessage();
+		}, 108000)
+
+	setMessage();
+
+	const dataChecker = setInterval(() => {
 		counter = counter + 1;
-		const time = new Date().toString();		// fetch some data
+		count = Math.floor(counter / 10);
+		const time = moment().format('HH:mm:ss');		// fetch some data
 
-		if (counter % 5 === 0) return onData(null, {time, msg});
+		if (counter % 1080 < 270) {
+			return onData(null, {time, msg, count});
+		} else {
+			onData(null, {time, null, count});		// onData receives error first
+		}
+	}, 250);		// repeat fetching
 
-		onData(null, {time});		// onData receives error first
-	}, 1000);		// repeat fetching
-
-	const cleanup = () => clearInterval(handler);
+	const cleanup = () => {
+		clearInterval(messageSetter);
+		clearInterval(dataChecker);
+	}
 	return cleanup;		// return a funciton to clean up resources
 }
 
 const loadingFxn = () => {
-	console.log('Hmming');
-
-	return (<div>Hmm...</div>);
+	return (<div className="center">Hmm...</div>);
 }
 
 const errorHandler = () => (<div>Apologies</div>);
