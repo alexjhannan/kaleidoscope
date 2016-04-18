945927bc7c9fc974c6b4d342c1d887836f5d30cf
alexjhannan <alexjhannan@gmail.com>
2016-04-17 22:09:05 -0400

    Fixed navbar side menu for mobile

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 
-export default Navbar = () =>(
-  <nav className="purple darken-4 small" role="navigation">
-    <div className="nav-wrapper container">
-      <a id="logo-container" href="/" className="brand-logo">Kaleidoscope</a>
-      
-      <ul className="right hide-on-med-and-down">
-        <li><a href="/wiseman">Wiseman</a></li>
-      </ul>
+export default Navbar = React.createClass({
+  componentDidMount() {
+    $(".button-collapse").sideNav();
+  },
+  render() {
+    return (
+      <nav className="purple darken-4 small" role="navigation">
+        <div className="nav-wrapper container">
 
-      <ul id="nav-mobile" className="side-nav">
-        <li><a href="/wiseman">Wiseman</a></li>
-      </ul>
+          <a href="/" className="brand-logo">Kaleidoscope</a>
 
-      <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
-    
-    </div>
-  </nav>
-);
+          <ul className="right hide-on-med-and-down">
+            <li><a href="/wiseman">Wiseman</a></li>
+            <li><a href="/glossary">Glossary</a></li>
+          </ul>
+
+          <ul id="slide-out" className="side-nav">
+            <li><a href="/wiseman">Wiseman</a></li>
+            <li><a href="/glossary">Glossary</a></li>
+          </ul>  
+
+          <a href="#" data-activates="slide-out" className="button-collapse"><i className="material-icons">menu</i></a>
+        </div>
+      </nav>
+    );
+  }
+})
+  
