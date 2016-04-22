ce6645afbdf2e6083fc1e7c335043b026631b31b
alexjhannan <alexjhannan@gmail.com>
2016-04-22 12:48:56 -0400

    Cleaned up design assets

--- client/components/characterBoard.jsx
+++ client/components/characterBoard.jsx
@@ -30,11 +30,12 @@ export default CharacterBoard = React.createClass({
         <div className="col s4 center">
           <a href="#"><img className="circle responsive-img z-depth-1 characterBoard--placeholderLink" src={char.avatar} /></a>
           <a href="#"><img className="circle responsive-img z-depth-1 characterBoard--placeholderLink" src={char.avatar} /></a>
-          <a href="#"><button className="characterBoard--testButton"><span className="characterBoard--testButtonText">LOGIN</span></button></a>
         </div>
 
         <div className="col s4">
-          <img className="z-depth-2 characterBoard--avatar" src={char.avatar} />
+          <div className="center">
+            <img className="z-depth-2 characterBoard--avatar" src={char.avatar} />
+          </div>
           <div className="card darken-1 center" id="mainCard">
             <p>NAME</p>
             <div className="divider"></div>
