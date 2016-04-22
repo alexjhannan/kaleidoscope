ff71ac8f635279e5e0a73efbf2f25b234918e1c8
alexjhannan <alexjhannan@gmail.com>
2016-04-21 23:05:43 -0400

    Updated navbar, ported an icon in the process

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -6,20 +6,24 @@ export default Navbar = React.createClass({
   },
   render() {
     return (
-      <nav className="purple darken-4 small" role="navigation">
+      <nav className="navbar--nav small" role="navigation">
         <div className="nav-wrapper container">
 
-          <a href="/" className="brand-logo">Kaleidoscope</a>
+          <a href="/" className="brand-logo center"><img src={"/icons/Kaleidoscope.png"} className="navbar--kaleidoscope z-depth-3" alt="Navbar Logo" /></a>
 
-          <ul className="right hide-on-med-and-down">
+          <ul className="left hide-on-med-and-down">
             <li><a href="/wiseman">Wiseman</a></li>
             <li><a href="/glossary">Glossary</a></li>
+          </ul>
+
+          <ul className="right">
+            <li>Kaleidoscope</li>
           </ul>
 
           <ul id="slide-out" className="side-nav">
             <li><a href="/wiseman">Wiseman</a></li>
             <li><a href="/glossary">Glossary</a></li>
-          </ul>  
+          </ul>
 
           <a href="#" data-activates="slide-out" className="button-collapse"><i className="material-icons">menu</i></a>
         </div>
