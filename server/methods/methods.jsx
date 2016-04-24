e27527207d83fd389535ef591b67ab560db08852
alexjhannan <alexjhannan@gmail.com>
2016-04-24 13:54:03 -0400

    Added methods for character management

--- server/methods/methods.jsx
+++ server/methods/methods.jsx
@@ -2,21 +2,18 @@ import {Meteor} from 'meteor/meteor';
 import moment from 'moment';
 import {GlossaryEntries, Characters} from '/lib/collections.jsx';
 
+// TODO: categorize methods by data concern when file starts getting unwieldy
+
+// current standard practice is to return the mongo function being called, should remove before production
 
 Meteor.methods({
   // methods for characters
   deleteCharacter (_id) {
     return Characters.remove(_id);
   },
-  createNewCharacter () {
-    return Characters.insert({ 
-      createdAt: new moment().format('MMM DD, YYYY'),
-      name: 'Name',
-      avatar: 'http://lorempixel.com/250/250/nature/1',
-      element: 'Unchosen',
-      expertise: 'TBA',
-      energies: 'TBA'
-    });
+  insertCharacter (attributes) {
+    attributes.createdAt = new moment().format('MMM DD, YYYY');
+    return Characters.insert(attributes);
   },
   updateCharacter(_id, update) {
     return Characters.update(_id, {$set: update});
