70b991da2c638e3f855af5f15858883b1dd7dc7b
alexjhannan <alexjhannan@gmail.com>
2016-04-26 11:04:02 -0400

    Moved styles to main

--- client/components/mustLogIn.jsx
+++ client/components/mustLogIn.jsx
@@ -5,9 +5,9 @@ export default MustLogIn = () => (
 	<div className="container">
 		<div className="row">
 			<div className="center">
-				<div className="card mustLogIn--card">
-					<div className="card-content">
-						<span className="card-title white-text">Can't Access</span>
+				<div className="card general--largeCard">
+					<div className="card-content white-text">
+						<span className="card-title">Can't Access</span>
 						<p className="white-text">Sorry, you need to log in before you can access this page.</p>
 					</div>
 					<div className="card-action">
